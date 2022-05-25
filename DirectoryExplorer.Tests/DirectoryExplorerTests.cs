c553e76978a63b4ca621094e15e813e250039098
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 11:49:59 +0530

    first commit

---
+++ DirectoryExplorer.Tests/DirectoryExplorerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using MagicFilesLib;
+
+namespace DirectoryExplorer.Tests
+{
+    [TestFixture]
+    public class DirectoryExplorerTests
+    {
+        private readonly string _file1 = "file.txt";
+        private readonly string _file2 = "file2.txt";
+        Mock<IDirectExplorer> d;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+          d = new Mock<IDirectExplorer>();
+        }
+
+        [Test]
+        public void DirectoryExplorer_Test()
+        {
+            
+            d.Setup(obj => obj.GetFiles());
+            var directoryExplorer = d.Object;
+            var explorer = new DirectExplorer();
+            var result = explorer.GetFiles();
+            Assert.That(result,Is.Not.Null);
+            Assert.That(result, Has.Count.EqualTo(2));
+            CollectionAssert.Contains(result, _file1);
+        }
+
+    }
+}
---
+++ MagicFilesLib/DirectExplorer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MagicFilesLib
+{
+    public interface IDirectExplorer
+    {
+        ICollection<string> GetFiles();
+    }
+    public class DirectExplorer:IDirectExplorer
+    {
+        IList<string> list = new List<string>();
+        public DirectExplorer()
+        {
+            list.Add("file.txt");
+            list.Add("file1.txt");
+            
+
+        }
+
+        public ICollection<string> GetFiles()
+        {
+            return list;
+        }
+       /* public ICollection<string> GetFiles(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+            return files;
+        } */
+
+    }
+}
